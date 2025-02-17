271e2b04d956131a6149e23209be303d2bf5e82e
이건호 <ggamea100@gmail.com>
2025-02-17 17:05:03 +0900

    v0.11 choice

--- day06.py
+++ day06.py
@@ -1,10 +1,18 @@
+import random as rd
+
+
 try:
-    input_file=input("File name: ")
-    fp=open(input_file,'r')
-    readme_list=fp.readlines()
-    rls = readme_list[0].split('_') #읽어서 _를 기준으로 나눔
-    print(readme_list)
-    print(rls)
-    fp.close()
+    with open("students.csv",'r') as fp:
+        open_list=fp.readlines()
+        open_list.remove("이상혁\n")
+        open_list.remove("조윤하\n")
+        open_list.remove("김철중n")
+        open_list.remove("김현민\n")
+        open_list.remove("김찬빈\n")
+        n=int(input('뽑을 사람의 수:'))
+        for i in range(n):
+            randpick=rd.choice(open_list)
+            print(randpick, end='')
+            open_list.remove(randpick)
 except FileNotFoundError as err:
-    print(f"{input_file} is not exist. {err}")
+    print(err)
