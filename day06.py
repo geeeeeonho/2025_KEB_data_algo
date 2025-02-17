97af81feae326c90a1250653aa86d74d5c30460d
이건호 <ggamea100@gmail.com>
2025-02-17 11:37:42 +0900

    v0.5 int++

--- day06.py
+++ day06.py
@@ -1,12 +1,7 @@
-def dec_oct(n) -> int:
-    '''
-    8진수 변환
-    :return: 8진수값
-    '''
-    if n==0:
-        return ""
-    else:  #8의 자릿수마다 더해서 계산 (64->100 + (8->10 + (under -> noremal ))
-        return dec_oct(n//8) + str(n%8)
-
-n=int(input("8진수로 바꿀 수 :"))
-print("==>", dec_oct(n))
+#f(n) = 3 + n
+#0(n)
+n=int(input("1까지 합칠 수 :"))
+sum=0
+for i in range(n+1):
+   sum+=i
+print(sum)
