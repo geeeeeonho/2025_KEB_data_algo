c8127ef644b506e40bf80a237d1a4a594a8820d1
이건호 <ggamea100@gmail.com>
2025-02-17 09:50:52 +0900

    v0.3 even oper with BIT

--- day06.py
+++ day06.py
@@ -1,20 +1,10 @@
-# def is_even(n) -> bool:
-#     """
-#     짝수 판정 함수
-#     :param n: 판정할 함수
-#     :return: True(짝수면) , False(홀수면)
-#     """
-#     if n%2==0:
-#         return True
-#     else:
-#         return False
-#
-# n=int(input("숫자입력: "))
-# print(is_even(n))
+def is_even(n) -> bool:
+    """
+    짝수 판정 함수
+    :param n: 판정할 함수
+    :return: True(짝수면) , False(홀수면)
+    """
+    return not n&1
 
-a=10
-b=11
-#bit operation(0000 0000)
-print(a&b)  #비트 and 연산
-print(a|b)  #비트 or 연산
-print(a^b)  #비트 서로 다르면 1
+n=int(input("숫자입력: "))
+print(is_even(n))
