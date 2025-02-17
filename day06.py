df90a7d63fac540b52569073d07572fb345eddf0
이건호 <ggamea100@gmail.com>
2025-02-17 14:18:23 +0900

    v0.8 X^n ++

--- day06.py
+++ day06.py
@@ -1,34 +1,33 @@
+#식 출력
 def print_poly(f_x, t_x) -> str:
-    #term = len(f_x) - 1
     poly_expression = "f(x) = "
 
     for i in range(len(fx)):
         coefficient = f_x[i]
         term = t_x[i]
 
-        if coefficient >= 0:
+            # 기호 설정          #맨 앞에는 +를 처리X
+        if coefficient >= 0 and i !=0:
             poly_expression = poly_expression + "+"
         poly_expression = poly_expression + f'{coefficient}x^{term} '
-        #term = term - 1
-
     return poly_expression
 
 
+#x대입해서 계산
 def calculation_poly(x_value, f_x, t_x) -> int:
     return_value = 0
-    # term = len(f_x) - 1
 
     for i in range(len(fx)):
         coefficient = f_x[i]
         term = t_x[i]
+        # x앞*(대입 수^차수)
         return_value += coefficient * pow(x_value, term)
-        # term = term - 1
 
     return return_value
 
 
-fx = [2, 5, -9, 11]
-tx = [20, 7, 2, 0]
+fx = [2, 5, -9, 11] #x앞
+tx = [20, 7, 2, 0]  #차수
 
 if __name__ == "__main__":
     print(print_poly(fx, tx))
