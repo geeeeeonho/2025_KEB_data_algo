48b7145532879a7ef6a3c3249bab6cd3a5122bd7
이건호 <ggamea100@gmail.com>
2025-02-18 11:31:29 +0900

    day07 v0.6 linkedlist search

--- day07.py
+++ day07.py
@@ -1,10 +1,46 @@
-from collections import deque
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
 
-d= deque()
-d.append(7)
-d.append(-11)
-d.append(8)
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next:  # if next node exist
+            current = current.next  # move
+        current.next = Node(data)
+
+
+    def search(self,target):
+        current = self.head
+        while current.next:
+            if current.data == target:
+                return True
+            else:
+                current = current.next
+        return False
+
+
+    def __str__(self):  #출력에 대한 설정
+        node = self.head
+        while node is not None: #동일 node != None
+            print(node.data)#노드 출력
+            node=node.next  #다음 노드로
+        return "end"
+
 
 if __name__ == "__main__":
-    for data in d:
-        print(data)
+    l = LinkedList()
+    l.append(7)
+    l.append(-11)
+    l.append(8)
+    print(l)
+    print(l.search(999))
+    print(l.search(-11))
