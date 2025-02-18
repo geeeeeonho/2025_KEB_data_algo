50da100d532b4868da65eb44d0dccaecc575d9ca
이건호 <ggamea100@gmail.com>
2025-02-18 11:26:23 +0900

    day07 v0.5 deque

--- day07.py
+++ day07.py
@@ -1,34 +1,10 @@
-class Node:
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
+from collections import deque
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-
-    def append(self, data):
-        if not self.head:
-            self.head = Node(data)
-            return
-        current = self.head
-        while current.next:  # if next node exist
-            current = current.next  # move
-        current.next = Node(data)
-
-
-    def __str__(self):  #출력에 대한 설정
-        node = self.head
-        while node is not None: #동일 node != None
-            print(node.data)#노드 출력
-            node=node.next  #다음 노드로
-        return "end"
-
+d= deque()
+d.append(7)
+d.append(-11)
+d.append(8)
 
 if __name__ == "__main__":
-    l = LinkedList()
-    l.append(7)
-    l.append(-11)
-    l.append(8)
-    print(l)
+    for data in d:
+        print(data)
