7a16dbd369972808f402765f4bdfda78b840f060
이건호 <ggamea100@gmail.com>
2025-02-18 10:08:44 +0900

    day7 v0.3 stack debugger

--- day07.py
+++ day07.py
@@ -1,26 +1,17 @@
-class Stack:
-    def __init__(self): #스택 생성
-        self.items=[]
+def a(i):
+    j=9
+    print(i,j)
 
-    def push(self , data):  #실질 삽입
-        self.items.append(data)
 
-    def pop(self):  #팝(리턴 후 삭제)
-        return self.items.pop()
+def b(k):
+    a(1)
+    print(k)
 
-    def size(self): #스택 안의 아이템 크기 재기
-        return len(self.items)
 
-    def peek(self)->object: #픽(이전 거를 보기)
-        return self.items[-1]
+def main():
+    print("start")
+    b(3)
+    print("end")
 
-    def is_empty(self)->bool:   #비어 있는지 확인
-        return len(self.items) == 0
-
-s1=Stack()
-s1.push(0)
-s1.push(1)
-s1.push(2)
-print(s1.pop())
-print(s1.peek())
-print(s1.is_empty())
+if __name__ == "__main__":
+    main()
