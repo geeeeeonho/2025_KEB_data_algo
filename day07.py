09011b3920cc94486ab59c4f0931e7e87ac03389
이건호 <ggamea100@gmail.com>
2025-02-18 15:29:46 +0900

    day07 v0.9 queue

--- day07.py
+++ day07.py
@@ -1,21 +1,48 @@
-def check_bracket(expr: str) -> bool:
-    """
-    check bracket in expression.
-    :param expr: str
-    :return: bool
-    """
-    stack = list()
-    table = {')': '(', ']': '[', '}': '{', '>': '<'}
-    for char in expr:
-        # if char not in table:
-        if char in table.values():  # 여는 기호"([{<"
-            stack.append(char)  #스텍에 여는 기호 저장
-        elif char in table.keys():  # 닫는 기호 ")]}>"
-            if not stack or table[char] != stack.pop():
-                return False  # 닫는 괄호가 짝이 맞지 앉을 떄
-    return len(stack) == 0  # 마지막에 서로 짝이 맞지 않아서 스택에 남은 경우
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data    #데이터
+        self.next = next    #포인터
+
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self._size = 0
+
+
+    def enqueue(self, data):
+        self._size+=1   #사이즈 1증가
+        node = Node(data)   #입력 받은 데이터로 노드를 추가
+
+        if self.rear is None: #삽입을 받는 위치가 None 이면
+            self.front = node #값을 다음 노드를 저장
+            self.rear = node #그 다음 노드로 이동
+        else:   #이미 위치가 존재한다면
+            self.rear.next =node #노드를 입력
+            self.rear = node #그 다음 노드로 이동
+
+
+    def dequeue(self):
+        if self.front is None: #꺼낼 게 없다면
+            raise IndexError('dequeue from empty queue')
+        self._size -= 1  # 사이즈 1감소
+        temp = self.front #백업
+        self.front=self.front.next #업데이트
+        if self.front is None:
+            self.rear = None
+        return temp.data
+
+
+    def size(self) -> int:
+        return self._size
 
 
 if __name__ == "__main__":
-    expression = input("Input expression : ")
-    print(check_bracket(expression))
+    q = Queue()
+    q.enqueue(7)
+    q.enqueue(-11)
+    q.enqueue(8)
+    print('size=', q.size())
+    print(q.dequeue())
+    print('size=',q.size())
