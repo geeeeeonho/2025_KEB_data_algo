f3fdb6094a100711920df2a0cddf4ebfb6ea1b6c
이건호 <ggamea100@gmail.com>
2025-02-18 11:19:24 +0900

    day07 v04 linked list

--- day07.py
+++ day07.py
@@ -1,17 +1,34 @@
-def a(i):
-    j=9
-    print(i,j)
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
 
-def b(k):
-    a(1)
-    print(k)
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next:  # if next node exist
+            current = current.next  # move
+        current.next = Node(data)
 
 
-def main():
-    print("start")
-    b(3)
-    print("end")
+    def __str__(self):  #출력에 대한 설정
+        node = self.head
+        while node is not None: #동일 node != None
+            print(node.data)#노드 출력
+            node=node.next  #다음 노드로
+        return "end"
+
 
 if __name__ == "__main__":
-    main()
+    l = LinkedList()
+    l.append(7)
+    l.append(-11)
+    l.append(8)
+    print(l)
