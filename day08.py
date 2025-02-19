bb1f2e4d6ae53a203af120694eca97dee00dcc66
이건호 <ggamea100@gmail.com>
2025-02-19 14:08:39 +0900

    day08 v0.4 tree+search+del

--- day08.py
+++ day08.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     node.data=groups[0] #루트 작업
     root = node     #초기화 작업
 
+    #데이터 만들기
     for group in groups[1:]:
         node = TreeNode()
         node.data = group
@@ -33,7 +34,7 @@ if __name__ == "__main__":
     print("구성완료")
 
     # 데이터 찾기
-    find_group = input()
+    find_group = input("찾을 그룹명을 입력하시오:")
     current = root
     while True:
         if find_group == current.data:
@@ -45,7 +46,57 @@ if __name__ == "__main__":
                 break
             current = current.left
         else:
+
             if current.right is None:
                 print(f'{find_group}이(가) 존재하지 않습니다.')
                 break
             current = current.right
+
+    #데이터 삭제
+    del_group = input("삭제할 그룹명을 입력하시오:")
+    current = root #시작을 루트로
+    parent = None #부모노드를 지정
+    while True:
+        #a.삭제하려는 값과 현재 노드의 값이 일치 할때
+        if del_group == current.data:
+            #자식이 없는 경우
+            if current.left == None and current.right == None:
+                if parent.left == current:#자신이 부모의 왼쪽에 있다면
+                    parent.left = None
+                else:                     #자신이 부모의 오른쪽에 있다면
+                    parent.right =None
+                del(current)
+
+            #자식이 좌측에 있을 경우
+            elif current.left != None and current.right == None:
+                if parent.left == current:#자신이 부모의 왼쪽에 있다면
+                    parent.left = current.right#자식의 위치를 부모의 방향에
+                else:                     #자신이 부모의 오른쪽에 있다면
+                    parent.left = current.right#자식의 위치를 부모의 방향에
+                del(current)
+
+            #자식이 우측에 있을 경우
+            elif current.left == None and current.right != None:
+                if parent.left == current:#자신이 부모의 왼쪽에 있다면
+                    parent.left = current.right#자식의 위치를 부모의 방향에
+                else:                     #자신이 부모의 오른쪽에 있다면
+                    parent.left = current.right#자식의 위치를 부모의 방향에
+                del(current)
+
+            print(del_group,'이(가) 삭제됨.')
+            break
+
+        # a.삭제하려는 값 < 현재 노드의 값
+        elif del_group < current.data:
+            if current.left == None:
+                print(del_group, '이(가) 트리에 없음')
+                break
+            parent =current
+            current =current.left
+        # a.삭제하려는 값 > 현재 노드의 값
+        else:
+            if current.right == None:
+                print(del_group, '이(가) 트리에 없음')
+                break
+            parent =current
+            current =current.right
