57ce07679d0a4f0a34633645ffa004a06f05f74b
이건호 <ggamea100@gmail.com>
2025-02-20 11:08:32 +0900

    day09 v0.3 tree short

--- day09.py
+++ day09.py
@@ -4,46 +4,32 @@ class TreeNode: #각 노드 [<-,값(벨류),->]
 		self.data = None
 		self.right = None
 
-#삽입(내부에서 반복 필요X)
+
+#삽입(재귀함수 이용)
 def insert(root, value):
-    new_node = TreeNode()
-    new_node.data = value
+    if root is None:    #현위치에 위치한 값이 없을 경우
+        node = TreeNode()
+        node.data = value
+        return node
 
-    if root is None: #첫 노드인 경우
-        return new_node
-
-    current = root #위치 지정 필요
-    while True:
-        if value < current.data:        #값이 현재 값보다 작은 경우
-            if current.left is None:    #현재 위치에 값이 없으면
-                current.left = new_node #현위치에 벨류 삽입
-                break
-            current = current.left  #좌로 이동
-        else:                           #값이 현재 값보다 큰 경우
-            if current.right is None:   #현재 위치에 값이 없으면
-                current.right = new_node#현위치에 벨류 삽입
-                break
-            current = current.right  #우로 이동
+    if value < root.data:   #삽입하려는 값<위치한 값
+        root.left = insert(root.left, value)#재귀함수(루트값에 루트의 왼쪽을)
+    else:                   #삽입하려는 값>위치한 값
+        root.right = insert(root.right, value)#재귀함수(루트값에 루트의 오른쪽을)
     return root
 
 
-#내용 찾기
+#내용 찾기(단축)
 def search(root, value):
-    current = root  #위치 지정
-    while True:
-        if value == current.data:
-            print(f"{value}을(를) 찾았습니다")
-            break
-        elif value < current.data: #검색값이 현재 값보다 작은 경우
-            if current.left is None:
-                print(f"{value}이(가) 존재하지 않습니다")
-                break
-            current = current.left #좌로 이동
-        else:
-            if current.right is None:#검색값이 현재 값보다 큰 경우
-                print(f"{value}이(가) 존재하지 않습니다")
-                break
-            current = current.right  #우로 이동
+    current = root
+    while current:
+        if value == current.data:   #찾음
+            return current
+        elif value < current.data:  #검색 값<위치한 값
+            current = current.left
+        else:                       #검색 값>위치한 값
+            current = current.right
+    return None #찾아도 없으면 None
 
 
 #내용 확인(후위순회)
@@ -59,34 +45,21 @@ if __name__ == "__main__":
     numbers = [10, 15, 8, 3, 9]
     root = None
 
+    #생성
     for number in numbers: #반복문으로 삽입
         root = insert(root, number)
 
+    #내용 출력
     print("BST 구성 완료")
     post_order(root)
 
+    #검색
     print()
-    find_number = int(input('값 찾기:'))
-    search(root, find_number)
-
-
-    # find_group = int(input())
-    #
-    # current = root
-    # while True:
-    #     if find_group == current.data:
-    #         print(f"{find_group}을(를) 찾았습니다")
-    #         break
-    #     elif find_group < current.data:
-    #         if current.left is None:
-    #             print(f"{find_group}이(가) 존재하지 않습니다")
-    #             break
-    #         current = current.left
-    #     else:
-    #         if current.right is None:
-    #             print(f"{find_group}이(가) 존재하지 않습니다")
-    #             break
-    #         current = current.right
+    find_number = int(input('값 찾기 :'))
+    if search(root, find_number):
+        print(f"{find_number}을(를) 찾았습니다")
+    else:
+        print(f"{find_number}이(가) 존재하지 않습니다")
 
 
 
